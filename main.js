c8161e2800de4c3c0b97a16dce589256d8ca6530
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-09-12 18:49:38 -0400

    Add render method rendering markers to game grid

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const gameBoard = (() => {
-  return {};
+  let board = Array(9).fill('.');
+  const cells = Array.from(document.querySelectorAll('.cell'));
+  const render = () => {
+    for (let i = 0; i < 9; i++) {
+      cells[i].textContent = board[i];
+    }
+  };
+
+  render();
+
+  return { cells };
 })();
 
-const displayController = (() => {
+const gameController = (() => {
   return {};
-});
+})();
 
 const Player = () => {
   return {};
